4d522bbcc41e527c781d558ab2ef82ac54f04752
Joyce Zheng <joycezheng96@gmail.com>
2026-06-02 15:58:08 -0400

    fixed RAG caching and deployment bug

--- RAG.py
+++ RAG.py
@@ -1,18 +1,19 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
-
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 import pandas as pd
 import os
+import streamlit as st
+
 
 # LOAD CSV
-
 def load_csv():
     return pd.read_csv("data/sales_data.csv")
 
-# LOAD PDF
+
+# LOAD PDFs
 def load_pdfs(folder="data/"):
     documents = []
 
@@ -23,7 +24,8 @@ def load_pdfs(folder="data/"):
 
     return documents
 
-# SPLIT DOCUMENTS
+
+# SPLIT
 def split_docs(documents):
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=500,
@@ -31,42 +33,36 @@ def split_docs(documents):
     )
     return splitter.split_documents(documents)
 
-# CREATE VECTOR DATABASE (FAISS)
+
+# EMBEDDINGS + VECTOR DB
 def create_vector_db(chunks):
     embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
 
-    vector_db = FAISS.from_documents(chunks, embeddings)
-    return vector_db
+    return FAISS.from_documents(chunks, embeddings)
 
-# BUILD RETRIEVER
+
+# BUILD RETRIEVER (CACHE IMPORTANT)
+@st.cache_resource
 def build_retriever():
     pdf_docs = load_pdfs()
     chunks = split_docs(pdf_docs)
-
     vector_db = create_vector_db(chunks)
-    retriever = vector_db.as_retriever(search_kwargs={"k": 3})
+    return vector_db.as_retriever(search_kwargs={"k": 3})
 
-    return retriever
 
-# RAG SEARCH FUNCTION
+# RAG FUNCTION
 def search_context(query, retriever, csv_data):
-    # -----------------------
-    # CSV CONTEXT
-    # -----------------------
+
     csv_context = csv_data.describe(include="all").to_string()
 
-    # -----------------------
-    # PDF CONTEXT (SEMANTIC SEARCH)
-    # -----------------------
-    docs = retriever.get_relevant_documents(query)
+    docs = retriever.invoke(query)
 
-    pdf_context = "\n\n".join([doc.page_content for doc in docs])
+    pdf_context = "\n\n".join(
+        [doc.page_content for doc in docs if doc.page_content]
+    )
 
-    # -----------------------
-    # FINAL COMBINED CONTEXT
-    # -----------------------
     return f"""
 CSV CONTEXT:
 {csv_context}
